a71133e5af9f10d8d3a494dbfbe99a0d924420c6
maomao <maomao@maomaos-MacBook-Pro.local>
2025-08-12 23:40:25 -0400

    Rough draft business card site

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,35 +1,34 @@
-import * as THREE from "three"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
-const App = () => {
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(75, 1 / 1, 0.1, 1000)
+export const initApp = () => {
+    gsap.registerPlugin(ScrollTrigger)
 
-    const renderer = new THREE.WebGLRenderer()
-    renderer.setSize(400, 400)
+    // gsap.to("#wavy-text", {
+    //     scrollTrigger: {
+    //         trigger: "#hero",
+    //         start: "top top",
+    //         end: "bottom 400",
+    //         scrub: 1,
+    //         // markers: true,
+    //     },
+    //     attr: { startOffset: "100%" },
+    //     ease: "none",
+    // })
 
-    const heroElement = document.querySelector("#hero-image")
+    // gsap.from("#camera-image > img", {
+    //     duration: 0.5,
+    //     attr: { startOffset: "0%" },
+    //     ease: "none",
 
-    if (!heroElement) {
-        return
-    }
-
-    heroElement.appendChild(renderer.domElement)
-
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    const cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
-
-    camera.position.z = 5
-
-    const animate = () => {
-        cube.rotation.x += 0.01
-        cube.rotation.y += 0.01
-
-        renderer.render(scene, camera)
-    }
-
-    renderer.setAnimationLoop(animate)
+    //     scrollTrigger: {
+    //         trigger: "#hero",
+    //         start: "top top",
+    //         end: "bottom 400",
+    //         scrub: 1,
+    //         // markers: true,
+    //     },
+    // })
 }
 
 // Load
