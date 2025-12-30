93b52a91e9607d62793c213ea36124bd9eec2693
mei-lu <lumeidev@gmail.com>
2025-12-29 23:05:33 -0500

    Cleanup - Component experiment deletion

--- src/scripts/components/instax.ts
+++
@@ -1,9 +0,0 @@
-export const Instax = () => {
-    const instaxElements = document.querySelectorAll(
-        "instax"
-    ) as NodeListOf<HTMLElement>
-
-    for (const instaxElement of instaxElements) {
-        instaxElement.addEventListener("click", () => {})
-    }
-}
